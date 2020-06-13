bad59b97d13910b64fb2d6e94027d2c60a5683c6
Jasmin Bom <jsmnbom@gmail.com>
2020-06-13 17:17:45 +0200

    Move vuetify into index.ts

--- src/options_ui/index.ts
+++ src/options_ui/index.ts
@@ -1,13 +1,32 @@
 import OptionsPage from './OptionsPage.vue';
 import Vue from 'vue';
-import vuetify from '@/plugins/vuetify';
+import Vuetify from 'vuetify/lib';
 
+/* Load vue-devtools through https reverse proxy proxy */
 if (process.env.NODE_ENV === 'development') {
   const vueDevToolsTag = document.createElement('script');
   vueDevToolsTag.setAttribute('src', 'https://localhost:8099');
   document.body.appendChild(vueDevToolsTag);
 }
 
+/* Set up Vuetify */
+Vue.use(Vuetify);
+
+// Use user preferred theme that matches browser
+const userPrefersDark =
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+const vuetify = new Vuetify({
+  theme: {
+    dark: userPrefersDark,
+  },
+  icons: {
+    iconfont: 'mdiSvg',
+  },
+});
+
+/* Set up Vue */
 const appTag = document.createElement('div');
 appTag.id = 'app';
 document.body.appendChild(appTag);
