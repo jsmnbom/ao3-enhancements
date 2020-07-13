a76d686629c5d64f6b799f53f610dfa3a1777f8e
Jasmin Bom <jsmnbom@gmail.com>
2020-07-13 15:08:31 +0200

    Actually use the background script

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,5 +1,7 @@
 browser.runtime.onInstalled.addListener((details) => {
   if (details.reason == 'update') {
+    // Changed to cache.workPagesChecked and has
+    // other meaning so remove completely
     void browser.storage.local.remove('cache.kudosChecked');
   }
 });
