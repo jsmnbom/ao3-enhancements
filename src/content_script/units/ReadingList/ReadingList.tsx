9832b9e6c8268a05b1201a6286d0d6515e85bfc6
Jasmin Bom <jsmnbom@gmail.com>
2021-08-17 19:35:29 +0200

    Add missing space

--- src/content_script/units/ReadingList/ReadingList.tsx
+++ src/content_script/units/ReadingList/ReadingList.tsx
@@ -118,7 +118,7 @@ class ContentScriptWork extends BaseWork {
           {jump ? 'Jump to chapter' : 'Chapter'} {chapterIndex + 1}
           {jump ? '→' : ''}
         </a>
-        {!jump && this.chapters[chapterIndex].readText}
+        {!jump && ` ${this.chapters[chapterIndex].readText}`}
       </>
     );
   }
