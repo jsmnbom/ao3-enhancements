948aeb832c685dba7a078f18bf1c4da6f18f1dd3
Jasmin Bom <jsmnbom@gmail.com>
2025-09-14 16:29:27 +0200

    Fix issue when testing chrome extension with AO3 behind Cloudflare

--- scripts/web-ext.chrome.mjs
+++ scripts/web-ext.chrome.mjs
@@ -4,6 +4,9 @@ export default {
   ignoreFiles: ['**/*.map', '**/*.meta.json', '**/**.stats.html'],
   run: {
     target: ['chromium'],
+    // When AO3 is behind cloudflare, non cached requests sometimes refuse to load without this flag
+    // https://github.com/mozilla/web-ext/issues/3511
+    args: ['--disable-blink-features=AutomationControlled'],
     startUrl: [
       'https://archiveofourown.org/',
       'chrome://extensions/',
